308a4000e85b2d229e9c90954ecdb94ffe58f6dc
sanakdam <san.akdam@gmail.com>
2016-10-24 21:04:13 +0700

    remove false positive in several languages

--- error.js
+++ error.js
@@ -1,7 +1,12 @@
 const ignoredErrors = [
     "This sentence does not start with an uppercase letter",
     "Use a smart opening quote here: \"“\".",
-    "Use a smart closing quote here: \"”\"."
+    "Use a smart closing quote here: \"”\".",
+    "Cette phrase ne commence pas par une majuscule",
+    "Esa frase no se inicia con mayúscula",
+    "Esa frase no se inicia con mayúscula une majuscule",
+    "Propoziția nu începe cu literă mare",
+    "Sakinys turi prasidėti iš didžiosios raidės",
 ]
 
 module.exports =  class TranslationError {
